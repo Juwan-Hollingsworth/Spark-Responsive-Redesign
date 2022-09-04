8aeb7dd78f41313d2284fce6ab6fabab107f248d
Juwan <jhollingsworth5@student.clayton.edu>
2022-09-04 00:59:25 -0400

    remove unused logic

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,16 +3,6 @@ import "../header/header.css";
 import spark_logo from "../../assets/spark_logo3.svg";
 
 //On Scroll Header Logic
-window.onscroll = function () {
-  myFunction();
-};
-let header = document.getElementById("header");
-let sticky = header.offsetTop;
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else header.classList.remove("sticky");
-}
 
 function Header() {
   return (
