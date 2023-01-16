d47b3ff62cadfb4f071965a60d3c13c6b6060aae
Juwan <jhollingsworth5@student.clayton.edu>
2023-01-16 13:37:14 -0500

    Add container + elements 2 testimonial section

--- src/containers/testimonial/Testimonial.jsx
+++ src/containers/testimonial/Testimonial.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import "./testimonial.css";
 
 function Testimonial() {
-  return <div>Testimonial Card</div>;
+  return (
+    <div className="testimonial__main">
+      <div className="testimonial__container">
+        <h2>
+          Top class marketing agency! Juwan took the time to review my website
+          and web presence before coming up with an online marketing strategy
+          for my business. After implementing his road map my website traffic
+          has increased exponentially and my business has gained more clients as
+          a result. Thank you again for all your help. Great marketing partner!
+        </h2>
+        <img src="...."></img>
+        <h3>Chris Avond</h3>
+        <p>Home Improvement Services CEO</p>
+        <button className="caseStudy__button">See the Case Study</button>
+      </div>
+    </div>
+  );
 }
 
 export default Testimonial;
