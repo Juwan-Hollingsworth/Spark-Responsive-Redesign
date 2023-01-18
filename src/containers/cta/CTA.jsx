a3cd314d63226c7b69fe068ca01ce8d294f0e2d0
Juwan <jhollingsworth5@student.clayton.edu>
2023-01-18 13:57:21 -0500

    structure CTA section WIP

--- src/containers/cta/CTA.jsx
+++ src/containers/cta/CTA.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./cta.css";
+import ctaImg from "../../assets/cta_img.png";
 
 function CTA() {
-  return <div>CTA FORM 2 SECTION</div>;
+  return (
+    <div className="cta__main">
+      <div class="row">
+        <div class="column">
+          <img src={ctaImg} className="cta__img" alt="" />
+        </div>
+        <div class="column">
+          <h3>Digital Marketing Solutions</h3>
+          <form>
+            <label for="bname:">Business Name:</label>
+            <input type="text" id="bname" name="bname"></input>
+            <br />
+            <label for="bemail:">Business Email:</label>
+            <input type="text" id="bemail" name="bemail"></input>
+            <br />
+            <label for="bindustry:">Business Industry:</label>
+            <input type="text" id="bindustry" name="bindustry"></input>
+            <br />
+            <label for="bphone:">Phone:</label>
+            <input type="text" id="bphone" name="bphone"></input>
+            <br />
+            <label for="bmessage:">Message:</label>
+            <input type="text" id="bmessage" name="bmessage"></input>
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CTA;
